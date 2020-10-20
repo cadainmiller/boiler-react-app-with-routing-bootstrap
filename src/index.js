3d4e45e89874857790a28e2ee76ea8e54db3715c
Cadain <cmiller@aventmark.com>
2020-10-20 10:00:52 -0500

    Working on state with axios

--- src/index.js
+++ src/index.js
@@ -13,39 +13,31 @@ import Notfound from "./components/notfound";
 const routing = (
   <Router>
     <div>
-      <nav class="site-header sticky-top py-1">
-        <div class="container d-flex flex-column flex-md-row justify-content-between">
-          <a class="py-2" href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="d-block mx-auto"
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <line x1="14.31" y1="8" x2="20.05" y2="17.94"></line>
-              <line x1="9.69" y1="8" x2="21.17" y2="8"></line>
-              <line x1="7.38" y1="12" x2="13.12" y2="2.06"></line>
-              <line x1="9.69" y1="16" x2="3.95" y2="6.06"></line>
-              <line x1="14.31" y1="16" x2="2.83" y2="16"></line>
-              <line x1="16.62" y1="12" x2="10.88" y2="21.94"></line>
-            </svg>
-          </a>
-          <a class="py-2 d-none d-md-inline-block">
-            <Link to="/">Home</Link>
-          </a>
-          <a class="py-2 d-none d-md-inline-block">
-            <Link to="/users">Users</Link>
-          </a>
-          <a class="py-2 d-none d-md-inline-block">
-            <Link to="/contact">Contact</Link>
-          </a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">Navbar</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/users">Users</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
       </nav>
       <Switch>
