50bc2225ca62cafd8a315b85b34ee9e3f4648f62
Wild Mobile <cadainmiller@gmail.com>
2020-10-20 19:58:53 -0500

    Got his to work so I'm able to display data

--- src/components/users.js
+++ src/components/users.js
@@ -17,18 +17,41 @@ const axios = require("axios");
 // }
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    persons: [],
+  };
 
+  componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
-      this.state = {
-        data: res.data
-      };
+      const persons = res.data;
+      console.log(persons);
+      this.setState({ persons });
     });
   }
-
   render() {
-  return <div className="p-4">The data:</div>;
+    return (
+      <div className="p-4">
+        {this.state.persons.map((person) => (
+          <div>
+            <div className="card m-2">
+              <div className="card-header">{person.name}</div>
+              <div className="card-body">
+                <blockquote className="blockquote mb-0">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer posuere erat a ante.
+                  </p>
+                  <footer className="blockquote-footer">
+                    {person.name} 
+                    <cite title="Source Title"> Source Title</cite>
+                  </footer>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 export default Users;
