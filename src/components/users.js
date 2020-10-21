0d7e126d43f090a7105bbb44ea81903392df5275
Wild Mobile <cadainmiller@gmail.com>
2020-10-20 19:34:20 -0500

    Getting a headache from this

--- src/components/users.js
+++ src/components/users.js
@@ -2,23 +2,33 @@ import React from "react";
 
 const axios = require("axios");
 
-async function getUser() {
-  try {
-    await axios
-      .get(`https://jsonplaceholder.typicode.com/users`)
-      .then((res) => {
-        var persons = res.data;
-        console.log(persons);
-        return persons;
-      });
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function getUser() {
+//   try {
+//     await axios
+//       .get(`https://jsonplaceholder.typicode.com/users`)
+//       .then((res) => {
+//         var persons = res.data;
+//         console.log(persons);
+//         return persons;
+//       });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
 class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    axios.get(`https://jsonplaceholder.typicode.com/users`).then((res) => {
+      this.state = {
+        data: res.data
+      };
+    });
+  }
+
   render() {
-    
-    return <div className="p-4"></div>;
+  return <div className="p-4">The data:</div>;
   }
 }
 export default Users;
